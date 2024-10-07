35ef5da18c4ccb3f4a52af2b5811014463fe8c37
Iganza-roy <iganzaroy55@gmail.com>
2024-10-07 19:36:21 +0300

    Refactor project structure, update styles, and add Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,45 @@
+import logo from '../assets/logo_light.png';
+import { NavLink, useNavigate } from 'react-router-dom';
+
 const Navbar = () => {
-  return <div>NAVBAR</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-slate-800 '>
+      <img
+        onClick={() => navigate('/')}
+        className='w-40 cursor-pointer hover:-translate-y-0.5 transition-transform duration-300'
+        src={logo}
+        alt='logo'
+      />
+      <ul className='hidden md:flex items-start gap-5 font-medium text-light'>
+        <NavLink to='/'>
+          <li className='py-1'>Home</li>
+          <hr className='border-none outline-none h-0.5 bg-blue w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/crew'>
+          <li className='py-1'>Crew</li>
+          <hr className='border-none outline-none h-0.5 bg-blue w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/about'>
+          <li className='py-1'>About</li>
+          <hr className='border-none outline-none h-0.5 bg-blue w-3/5 m-auto hidden' />
+        </NavLink>
+        <NavLink to='/contact'>
+          <li className='py-1'>Contact</li>
+          <hr className='border-none outline-none h-0.5 bg-blue w-3/5 m-auto hidden' />
+        </NavLink>
+      </ul>
+      <div className='flex items-center gap-4'>
+        <button
+          onClick={() => navigate('/login')}
+          className='text-light px-8 py-3 rounded-md border-blue border-solid font-light hidden md:block hover:bg-blue hover:-translate-y-1 transition-colors-transform duration-300 active:bg-slate-900'
+        >
+          Get started
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#020617',
+        light: '#f1f5f9',
+        slate400: '#94a3b8',
+        slate600: '#7a8a9e',
+        slate800: '#334460',
+        blue: '#2563eb',
+        pink: '#db2778',
+      },
+    },
   },
   plugins: [],
 };
