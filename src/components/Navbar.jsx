673c7558fb451200687f88b33b7a99a88316443d
Iganza-roy <iganzaroy55@gmail.com>
2024-10-07 23:46:46 +0300

    Refactor Navbar component and add user profile dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import logo from '../assets/logo_light.png';
 import { NavLink, useNavigate } from 'react-router-dom';
+import user_1 from '../assets/user_1.jpg';
+import dropIcon from '../assets/dropdown_icon.svg';
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const [dropDown, setDropDown] = useState(false);
+  const [token, setToken] = useState(true);
 
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-slate-800 '>
@@ -12,7 +18,7 @@ const Navbar = () => {
         src={logo}
         alt='logo'
       />
-      <ul className='hidden md:flex items-start gap-5 font-medium text-light'>
+      <ul className='hidden md:flex items-start gap-10 font-bold text-base text-light flex-grow justify-center'>
         <NavLink to='/'>
           <li className='py-1'>Home</li>
           <hr className='border-none outline-none h-0.5 bg-blue w-3/5 m-auto hidden' />
@@ -31,12 +37,35 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-4'>
-        <button
-          onClick={() => navigate('/login')}
-          className='text-light px-8 py-3 rounded-md border-blue border-solid font-light hidden md:block hover:bg-blue hover:-translate-y-1 transition-colors-transform duration-300 active:bg-slate-900'
-        >
-          Get started
-        </button>
+        {token ? (
+          <div className='flex items-center gap-2 cursor-pointer group relative '>
+            <img className='w-8 rounded-full' src={user_1} alt='user' />
+            <img className='w-2.5' src={dropIcon} alt='drop down' />
+            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-light z-20 hidden group-hover:block transition-all'>
+              <div className='min-w-48 bg-slate-800 rounded flex flex-col gap-4 p-4'>
+                <p
+                  onClick={() => navigate('/user-profile')}
+                  className='hover:text-blue cursor-pointer'
+                >
+                  My profile
+                </p>
+                <p
+                  onClick={() => setToken(false)}
+                  className='hover:text-blue cursor-pointer'
+                >
+                  Logout
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <button
+            onClick={() => navigate('/login')}
+            className='text-light px-8 py-3 rounded-md border-blue border-solid font-light hidden md:block hover:bg-blue hover:-translate-y-1 transition-colors-transform duration-300 active:bg-slate-900'
+          >
+            Get started
+          </button>
+        )}
       </div>
     </div>
   );
