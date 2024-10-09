53c6f827937edd8c994a963339dca17802a36984
Iganza-roy <iganzaroy55@gmail.com>
2024-10-09 13:04:37 +0300

    Refactor project structure and remove unnecessary files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,55 @@
+import studio from '../assets/studio.jpg';
+
 const About = () => {
-  return <div>ABOUT</div>;
+  return (
+    <div>
+      <div className='flex flex-col justify-center items-center md:flex-col lg:flex-row'>
+        <div className='md:w-full lg:w-1/2 flex flex-col items-start justify-center gap-6 py-10 m-auto md:py-[4vw]'>
+          <h1 className='relative text-light font-bold text-3xl lg:text-4xl text-shadow-sm before:absolute before:rounded-md before:-bottom-[0.1px] before:left-0 before:w-full before:h-[13px] before:bg-blue before:-z-10  hover:before:bg-pink'>
+            About KoPeRo
+          </h1>
+          <p className='text-slate-400'>
+            Kopero Studios is a creative powerhouse dedicated to capturing
+            life’s most meaningful moments through photography and videography.
+            With a passion for storytelling and a keen eye for detail, we
+            specialize in delivering high-quality, personalized content that
+            reflects the unique essence of every subject. Whether it is
+            portraits, events, or street scenes, Kopero Studios brings a blend
+            of professionalism and artistry to every project, ensuring memorable
+            and impactful visuals for all our clients.
+          </p>
+          <div>
+            <h2 className='text-light font-bold text-2xl'>Message:</h2>
+            <p className='text-slate-400  py-3 border-b-4 border-b-slate-800'>
+              We’re excited to work with you and capture moments that will last
+              a lifetime. Our team is dedicated to delivering high-quality
+              photos and videos that reflect your unique vision
+            </p>
+          </div>
+          <div>
+            <h2 className='text-light font-bold text-2xl'>Vision:</h2>
+            <p className='text-slate-400 py-3'>
+              To be the leading creative hub for photography and videography,
+              where every moment is artfully captured and every story is
+              uniquely told. Our vision is to inspire and connect through
+              impactful visuals, offering an unparalleled experience in
+              creativity and professionalism for all our clients.
+            </p>
+          </div>
+        </div>
+        <div className='relative group '>
+          <div className='hidden lg:block absolute -top-[50px] left-[300px] h-[500px] w-1/2 bg-blue rounded-xl transition-color duration-500 group-hover:bg-pink z-[-1]'></div>
+          <div className='absolute inset-0 bg-blue opacity-10 group-hover:opacity-40 group-hover:shadow-lg group-hover:shadow-blue/30 rounded-xl transition-color duration-500 group-hover:scale-105 z-10' />
+          <img
+            src={studio}
+            alt=''
+            className='h-[250px] w-[2000px] lg:h-[400px] lg:w-[500px] object-cover rounded-xl transition-transform duration-500 group-hover:scale-105'
+          />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default About;
