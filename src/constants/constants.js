7883ed763fc0acc13dd29db8d98a792413052b33
Iganza-roy <iganzaroy55@gmail.com>
2024-10-10 15:25:53 +0300

    Refactor Service component to dynamically display service categories and selected service details

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -19,6 +19,14 @@ import performance from '../assets/performance.jpg';
 import portrait from '../assets/portrait.jpg';
 import street from '../assets/street.jpg';
 import wedding from '../assets/wedding.jpg';
+import wedding_2 from '../assets/wedding_2.jpg';
+import street_2 from '../assets/street_2.jpg';
+import portrait_1 from '../assets/portrait_1.jpg';
+import party_1 from '../assets/party_1.jpg';
+import graduation_2 from '../assets/graduation_2.jpg';
+import family_1 from '../assets/family_1.jpg';
+import dinner_1 from '../assets/dinner_1.jpg';
+import birthday_1 from '../assets/birthday_1.jpg';
 
 export const servicesData = [
   {
@@ -88,42 +96,58 @@ export const servicesData = [
 
 export const ServicesPageData = [
   {
+    image: street_2,
+    Category: 'Street Photography',
     Title: 'Street Moments',
     Description:
-      'We capture candid moments of urban life with a focus on real, unscripted interactions. From cityscapes to everyday people, we turn fleeting scenes into timeless images that reflect the energy and authenticity of the streets. Our street videography adds motion and sound, creating immersive videos that tell powerful stories. Whether fast-paced or quiet moments, we use creative techniques to document the raw beauty of urban environments, delivering visually engaging narratives.',
+      'We capture candid moments of urban life with a focus on real, unscripted interactions. From city scapes to everyday people, we turn fleeting scenes into timeless images that reflect the energy and authenticity of the streets.  Our street videography adds motion and sound, creating immersive videos that tell powerful stories. Whether fast-paced or quiet moments, we use creative techniques to document the raw beauty of urban environments, delivering visually engaging narratives.',
   },
   {
+    image: dinner_1,
+    Category: 'Dinner',
     Title: 'Dine & Shine',
     Description:
-      'We specialize in capturing the elegance of dining experiences, from intimate dinners to large gatherings. Our photographers highlight the fine details—plated dishes, candlelit tables, and the joy shared over meals. Every image tells a story of flavor, ambiance, and connection. For videography, we create cinematic moments that bring your dining events to life, from the preparation of the first course to the final toast. We focus on the smiles, laughter, and culinary artistry that make your event unforgettable.',
+      'We specialize in capturing the elegance of dining experiences, from intimate dinners to large gatherings. Our photographers highlight the fine details—plated dishes, candlelit tables, and the joy shared over meals. Every image tells a story of flavor, ambiance, and connection.  For videography, we create cinematic moments that bring your dining events to life, from the preparation of the first course to the final toast. We focus on the smiles, laughter, and culinary artistry that make your event unforgettable.',
   },
   {
+    image: wedding_2,
+    Category: 'Wedding',
     Title: 'Ever After',
     Description:
-      'Your wedding day deserves to be immortalized with a collection of images that tell the full story of your love and celebration. From the heartfelt moments of the ceremony to the candid reactions of family and friends, we ensure that every precious moment is preserved. Our wedding videography captures the emotion and joy of your big day, with cinematic shots that transport you back to each vow, dance, and toast. Relive the magic with videos that are as beautiful as your memories.',
+      'Your wedding day deserves to be immortalized with a collection of images that tell the full story of your love and celebration. From the heartfelt moments of the ceremony to the candid reactions of family and friends, we ensure that every precious moment is preserved.  Our wedding videography captures the emotion and joy of your big day, with cinematic shots that transport you back to each vow, dance, and toast. Relive the magic with videos that are as beautiful as your memories.',
   },
   {
+    image: graduation_2,
+    Category: 'Graduation',
     Title: 'Caps Off',
     Description:
-      'Celebrate your academic achievements with stunning graduation photos that capture this proud milestone. Whether individual portraits in your cap and gown or group shots with classmates, our photographers highlight the joy and excitement of your accomplishment. Our videography offers a unique way to preserve the energy of your graduation day, including the ceremony, speeches, and the moments shared with family and friends. We focus on the emotion and pride of this special time in your life.',
+      'Celebrate your academic achievements with stunning graduation photos that capture this proud milestone. Whether individual portraits in your cap and gown or group shots with classmates, our photographers highlight the joy and excitement of your accomplishment.  Our videography offers a unique way to preserve the energy of your graduation day, including the ceremony, speeches, and the moments shared with family and friends. We focus on the emotion and pride of this special time in your life.',
   },
   {
+    image: party_1,
+    Category: 'Corporate parties',
     Title: 'Corporate Celebrations',
     Description:
-      'We capture the essence of corporate events, from holiday parties to company celebrations, with a professional yet candid approach. Our photographers focus on documenting team interactions, entertainment, and key moments that make your corporate party stand out. With our videography services, we produce dynamic videos that showcase the energy and excitement of your event, from the opening remarks to the final celebration. Perfect for sharing within your organization or on social media.',
+      'We capture the essence of corporate events, from holiday parties to company celebrations, with a professional yet candid approach. Our photographers focus on documenting team interactions, entertainment, and key moments that make your corporate party stand out.  With our videography services, we produce dynamic videos that showcase the energy and excitement of your event, from the opening remarks to the final celebration. Perfect for sharing within your organization or on social media.',
   },
   {
+    image: birthday_1,
+    Category: 'Birthdays',
     Title: 'Birthday Bliss',
-    Description: `Whether it’s a child’s first birthday or a milestone celebration, we turn birthday moments into vibrant memories. Our photographers capture the laughter, decorations, cake, and joy shared among friends and family, ensuring that each moment is framed beautifully. Our videography services create engaging videos that allow you to relive the celebration, from the singing of 'Happy Birthday' to the cutting of the cake. We focus on the smiles, interactions, and special moments that make birthdays unforgettable`,
+    Description: `Whether it’s a child’s first birthday or a milestone celebration, we turn birthday moments into vibrant memories. Our photographers capture the laughter, decorations, cake, and joy shared among friends and family, ensuring that each moment is framed beautifully.  Our videography services create engaging videos that allow you to relive the celebration, from the singing of 'Happy Birthday' to the cutting of the cake. We focus on the smiles, interactions, and special moments that make birthdays unforgettable`,
   },
   {
+    image: family_1,
+    Category: 'Family Photos',
     Title: 'Family Bonds',
     Description:
-      'Family portraits are a timeless way to capture your loved ones together, whether in a formal setting or a casual outdoor shoot. We create beautiful images that reflect the bonds and personalities within your family, giving you memories to cherish for a lifetime. Our videography services offer a unique way to document family moments, with videos that showcase your family’s dynamic in an authentic and heartwarming way. Perfect for sharing with extended family or preserving for future generations',
+      'Family portraits are a timeless way to capture your loved ones together, whether in a formal setting or a casual outdoor shoot. We create beautiful images that reflect the bonds and personalities within your family, giving you memories to cherish for a lifetime.  Our videography services offer a unique way to document family moments, with videos that showcase your family’s dynamic in an authentic and heartwarming way. Perfect for sharing with extended family or preserving for future generations',
   },
   {
+    image: portrait_1,
+    Category: 'Portrait Pictures',
     Title: 'Portrait Perfection',
     Description:
-      'We specialize in creating professional and personal portraits that reflect the unique personality of each individual. From corporate headshots to creative lifestyle portraits, our photographers focus on making you look your best in every shot. Our portrait videography adds a dynamic touch, perfect for introducing yourself or showcasing your personal brand. Whether for business or personal use, we produce polished videos that highlight your strengths and individuality.',
+      'We specialize in creating professional and personal portraits that reflect the unique personality of each individual. From corporate headshots to creative lifestyle portraits, our photographers focus on making you look your best in every shot.  Our portrait videography adds a dynamic touch, perfect for introducing yourself or showcasing your personal brand. Whether for business or personal use, we produce polished videos that highlight your strengths and individuality.',
   },
 ];
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,5 +1,84 @@
+import { useState } from 'react';
+import { ServicesPageData } from '../constants/constants';
+import { FaHandshakeSimple } from 'react-icons/fa6';
+import { IoIosArrowForward } from 'react-icons/io';
+import photographer_1 from '../assets/photographer_1.jpg';
+
 const Services = () => {
-  return <div>SERVICES</div>;
+  // State to store the selected service data
+  const [selectedService, setSelectedService] = useState({
+    Title: 'Welcome to Kopero Studios',
+    Description:
+      'We specialize in delivering high-quality photography and videography services for all your needs. Select a service category to explore our tailored offerings, from candid street photography to elegant weddings and corporate events.',
+    image: photographer_1,
+  });
+
+  const handleClick = (categoryName, e) => {
+    e.preventDefault();
+
+    const service = ServicesPageData.find(
+      (service) => service.Category === categoryName
+    );
+
+    if (service) {
+      setSelectedService({
+        Title: service.Title,
+        Description: service.Description,
+        image: service.image,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div className='flex flex-col justify-center items-center gap-[70px]'>
+        <div className='bg-blue flex flex-col justify-center items-center py-[50px] w-full rounded-lg'>
+          <FaHandshakeSimple className='text-light text-5xl -rotate-45' />
+          <h1 className='text-light text-4xl font-bold'>Our Services</h1>
+        </div>
+        <div className='flex gap-[40px] justify-center w-full'>
+          <div className='flex flex-col gap-3'>
+            {ServicesPageData.map((service) => (
+              <div
+                className='flex justify-start items-center gap-3 w-[200px]'
+                key={service.Category}
+              >
+                <a href='#' onClick={(e) => handleClick(service.Category, e)}>
+                  {service.Category}
+                </a>
+                <IoIosArrowForward />
+              </div>
+            ))}
+          </div>
+          <div className='flex flex-col w-[800px] h-[500px] m-auto rounded-lg relative overflow-hidden'>
+            <img
+              src={selectedService.image}
+              className='absolute inset-0 w-full h-full object-cover rounded-lg'
+              alt={selectedService.Title}
+            />
+            <div className='absolute inset-0 bg-blue opacity-40 rounded-lg'></div>
+            <div className='relative flex flex-col gap-7 p-10'>
+              <h1 className='text-3xl font-bold z-10 text-shadow'>
+                {selectedService.Title}
+              </h1>
+              <div className='w-full h-full text-white top-[150px] left-[20px] z-10'>
+                {selectedService.Description.split('.  ').map(
+                  (paragraph, index) => (
+                    <p key={index} className='mb-8 text-xl text-shadow'>
+                      {paragraph}.
+                    </p>
+                  )
+                )}
+              </div>
+              <button className='bg-blue text-white py-2 px-4 rounded w-[150px]'>
+                Book Session
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
