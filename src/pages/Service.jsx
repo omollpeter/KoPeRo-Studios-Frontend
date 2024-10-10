9b2ec3d6f3b9c0b68488274649acab0275a0d2d2
Iganza-roy <iganzaroy55@gmail.com>
2024-10-10 16:05:23 +0300

    Refactor Service component to dynamically display service categories and selected service details

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -13,6 +13,8 @@ const Services = () => {
     image: photographer_1,
   });
 
+  const [isActive, setIsActive] = useState(false);
+
   const handleClick = (categoryName, e) => {
     e.preventDefault();
 
@@ -26,6 +28,7 @@ const Services = () => {
         Description: service.Description,
         image: service.image,
       });
+      setIsActive(categoryName);
     }
   };
 
@@ -36,29 +39,39 @@ const Services = () => {
           <FaHandshakeSimple className='text-light text-5xl -rotate-45' />
           <h1 className='text-light text-4xl font-bold'>Our Services</h1>
         </div>
-        <div className='flex gap-[40px] justify-center w-full'>
+        <div className='flex flex-col md:flex-row gap-[40px] justify-center w-full'>
           <div className='flex flex-col gap-3'>
             {ServicesPageData.map((service) => (
               <div
-                className='flex justify-start items-center gap-3 w-[200px]'
+                className='flex justify-start items-center gap-3 w-[200px] text-lg text-light hover:text-blue'
                 key={service.Category}
               >
-                <a href='#' onClick={(e) => handleClick(service.Category, e)}>
+                <a
+                  href='#cat'
+                  onClick={(e) => handleClick(service.Category, e)}
+                  className={`${
+                    isActive === service.Category ? 'text-blue' : 'text-light'
+                  }`}
+                >
                   {service.Category}
                 </a>
-                <IoIosArrowForward />
+                <IoIosArrowForward
+                  className={`${
+                    isActive === service.Category ? 'text-blue' : 'text-light'
+                  }`}
+                />
               </div>
             ))}
           </div>
-          <div className='flex flex-col w-[800px] h-[500px] m-auto rounded-lg relative overflow-hidden'>
+          <div className='flex flex-col lg:w-[800px] lg:h-[500px] m-auto rounded-lg relative overflow-hidden group shadow-md shadow-blue hover:shadow-none'>
             <img
               src={selectedService.image}
-              className='absolute inset-0 w-full h-full object-cover rounded-lg'
+              className='absolute inset-0 w-full h-full object-cover rounded-lg  group-hover:rotate-1 hover:scale-105 transition-all duration-300 ease-in-out hover:skew-x-2 hover:skew-y-1'
               alt={selectedService.Title}
             />
-            <div className='absolute inset-0 bg-blue opacity-40 rounded-lg'></div>
+            <div className='absolute inset-0 bg-blue opacity-40 rounded-lg group-hover:opacity-70 group-hover:shadow-lg group-hover:shadow-blue/30 group-hover:rotate-1 group-hover:scale-105 transition-all duration-300 ease-in-out hover:skew-x-2 hover:skew-y-1'></div>
             <div className='relative flex flex-col gap-7 p-10'>
-              <h1 className='text-3xl font-bold z-10 text-shadow'>
+              <h1 className='text-3xl font-bold z-10 text-shadow' id='cat'>
                 {selectedService.Title}
               </h1>
               <div className='w-full h-full text-white top-[150px] left-[20px] z-10'>
@@ -70,7 +83,7 @@ const Services = () => {
                   )
                 )}
               </div>
-              <button className='bg-blue text-white py-2 px-4 rounded w-[150px]'>
+              <button className='bg-blue text-white py-2 px-4 rounded w-[150px] group-hover:bg-slate-800'>
                 Book Session
               </button>
             </div>
