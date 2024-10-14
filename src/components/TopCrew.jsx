1aedcc66fa34e39ed39e881c69dcfaf5798e65c5
Iganza-roy <iganzaroy55@gmail.com>
2024-10-14 23:04:52 +0300

    Refactor crew data and TopCrew component to display top crew members

--- src/components/TopCrew.jsx
+++ src/components/TopCrew.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import { crewData } from '../constants/Crews_constants';
 
 const TopCrew = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-5 mb-10'>
       <div className='flex flex-col gap-3 justify-center items-center'>
         <h1 className='relative font-bold before:absolute before:rounded-md before:-bottom-[0.1px] before:left-0 before:w-full before:h-[13px] before:bg-blue before:-z-20 hover:before:bg-pink transition duration-300 text-white text-4xl'>
           Our Top Crew
         </h1>
         <p className='font-medium'>Have a glance at our Best</p>
       </div>
-      <div></div>
-      <div></div>
+      <div className='grid md:grid-cols-6 sm:grid-cols-2 gap-1 justify-center items-center cursor-pointer '>
+        {crewData.map((crew) => (
+          <div
+            key={crew.image}
+            className='flex flex-col justify-center items-center hover:-translate-y-5 transition-all duration-300'
+          >
+            <img
+              src={crew.image}
+              alt={crew.name}
+              className='w-32 rounded-full relative'
+            />
+            <h3 className='text-light mt-4'>{crew.name.toUpperCase()}</h3>
+            <p className='text-slate-400 text-sm'>{crew.cat}</p>
+          </div>
+        ))}
+      </div>
+      <div className='flex items-center justify-center'>
+        <button
+          onClick={() => navigate('/crews')}
+          className='bg-blue text-sm font-semibold sm:text-base text-light px-8 py-3 rounded-md mt-6 hover:scale-105 transition-all'
+        >
+          View all
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/constants/Crews_constants.js
@@ -0,0 +1,47 @@
+import crew_1 from '../assets/crew_1.jpg';
+import crew_2 from '../assets/crew_2.jpg';
+import crew_3 from '../assets/crew_3.jpg';
+import crew_4 from '../assets/crew_4.jpg';
+import crew_5 from '../assets/crew_5.jpg';
+import crew_6 from '../assets/crew_6.jpg';
+
+export const crewData = [
+  {
+    image: crew_1,
+    name: 'Ethan Cole',
+    cat: 'Photographer',
+    stars: '4',
+  },
+  {
+    image: crew_2,
+    name: 'Olivia Brooks',
+    cat: 'Videographer',
+    stars: '4',
+  },
+  {
+    image: crew_6,
+    name: 'Michael Travis',
+    cat: 'Photographer',
+    stars: '4',
+  },
+
+  {
+    image: crew_5,
+    name: 'Grace Foster',
+    cat: 'Photographer',
+    stars: '4',
+  },
+  {
+    image: crew_4,
+    name: 'Samuel Hayes',
+    cat: 'Photographer',
+    stars: '5',
+  },
+
+  {
+    image: crew_3,
+    name: 'Marcus Reed',
+    cat: 'Videographer',
+    stars: '5',
+  },
+];
