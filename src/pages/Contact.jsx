2f7cdefeb167f674db8e2b3b9c3fd046600e7373
Iganza-roy <iganzaroy55@gmail.com>
2024-10-17 16:10:54 +0300

    Refactor Contact component to add form submission functionality and handle form data

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,42 @@
+import React from 'react';
 import { FiMail } from 'react-icons/fi';
 import { IoLogoWhatsapp } from 'react-icons/io';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { FaLocationDot } from 'react-icons/fa6';
+import { useState } from 'react';
+import { toast } from 'sonner';
 
 const Contact = () => {
+  const [result, setResult] = React.useState('');
+
+  const handleNavigate = (webLink) => {
+    window.open(webLink, '_blank');
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult('Sending....');
+    const formData = new FormData(event.target);
+
+    formData.append('access_key', 'f622c96c-57d3-47e6-a296-d95ceefae30f');
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success('From submitted successfully');
+      setResult('Form Submitted Successfully');
+      event.target.reset();
+    } else {
+      console.log('Error', data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className='flex flex-col gap-10'>
       <div className='flex justify-center items-center'>
@@ -19,7 +52,12 @@ const Contact = () => {
               <p className='text-slate-400 text-lg cursor-pointer hover:text-blue'>
                 Location
               </p>
-              <p className='hover:text-blue cursor-pointer'>
+              <p
+                className='hover:text-blue cursor-pointer'
+                onClick={() =>
+                  handleNavigate('https://maps.app.goo.gl/mtzLV2hufrP5TyBy8')
+                }
+              >
                 P.O. Box 001-10000, KPR
                 <br />
                 Tom Mboya Strt, Nairobi
@@ -32,7 +70,12 @@ const Contact = () => {
               <p className='text-slate-400 text-lg cursor-pointer hover:text-blue'>
                 Whatsapp
               </p>
-              <p className='hover:text-blue cursor-pointer'>+2547000000000</p>
+              <p
+                className='hover:text-blue cursor-pointer'
+                onClick={() => handleNavigate('https://wa.me/+2547000000000')}
+              >
+                +2547000000000
+              </p>
             </div>
           </div>
           <div className='flex flex-row gap-4 justify-start items-center w-full'>
@@ -50,7 +93,10 @@ const Contact = () => {
               <p className='text-slate-400 text-lg cursor-pointer hover:text-blue'>
                 Email
               </p>
-              <p className='hover:text-blue cursor-pointer'>
+              <p
+                className='hover:text-blue cursor-pointer'
+                onClick={() => handleNavigate('https://mail.google.com')}
+              >
                 info@kopero.ac.ke
               </p>
             </div>
@@ -62,6 +108,7 @@ const Contact = () => {
             <form
               action=''
               className='flex flex-col gap-6 justify-center items-center'
+              onSubmit={onSubmit}
             >
               <div className='flex flex-row gap-4'>
                 <input
@@ -93,10 +140,10 @@ const Contact = () => {
                 type='submit'
                 className='bg-slate-900 rounded-md w-[100px] h-[40px] text-light transition:transform duration-300 hover:bg-slate-800 hover:border-3 hover:-translate-y-1'
               >
-                {' '}
                 Submit
               </button>
             </form>
+            <span>{result}</span>
           </div>
         </div>
       </div>
