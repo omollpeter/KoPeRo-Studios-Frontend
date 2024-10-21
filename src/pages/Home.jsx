09262eed3ccbe3b1dacbe6ddab9e6dc6a212c519
Iganza-roy <iganzaroy55@gmail.com>
2024-10-21 12:01:10 +0300

    Refactor Home component to remove unnecessary loading logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from 'react';
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Testimonials from '../components/Testimonials';
 import TopCrew from '../components/TopCrew';
-import Preloader from '../components/Preloader';
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const isFirstVisit = localStorage.getItem('isFirstVisit');
-
-    if (!isFirstVisit) {
-      localStorage.setItem('isFirstVisit', 'true');
-      const timer = setTimeout(() => {
-        setIsLoading(false);
-      }, 4000);
-
-      return () => clearTimeout(timer);
-    } else {
-      setIsLoading(false);
-    }
-  }, []);
-  
-  if (isLoading) {
-    return <Preloader />;
-  }
-
   return (
     <div>
       <Hero />
