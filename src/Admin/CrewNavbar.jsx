8c83dd99d08eb169c49963a14bc7a99b0da5b32e
Iganza-roy <iganzaroy55@gmail.com>
2024-10-24 17:21:23 +0300

    Refactor Navbar component styles for responsiveness

--- src/Admin/CrewNavbar.jsx
+++ src/Admin/CrewNavbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b border-b-slate-600'>
       <div className='flex items-center gap-2 text-sm'>
         <img
-          className='w-36 sm:w-40 cursor-pointer hover:-translate-y-0.5 transition-transform duration-300'
+          className='w-24 md:w-36 cursor-pointer hover:-translate-y-0.5 transition-transform duration-300'
           src={logo}
           alt='logo'
         />
@@ -27,7 +27,7 @@ const Navbar = () => {
         </p>
       </div>
       <button
-        className='bg-blue text-light text-md px-10 py-2 rounded-full hover:bg-pink transition-all'
+        className='bg-blue text-light text-md px-6 md:px-10 py-2 rounded-full hover:bg-pink transition-all'
         onClick={() => handleLogout()}
       >
         Logout
